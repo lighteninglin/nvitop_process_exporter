f76a53c64880476c3af6ae5eb061ac7235183c28
LinYancheng <lyc2021@stu.xjtu.edu.com>
2023-05-24 18:21:58 +0800

    修复同一进程占用多张卡只显示一个的bug

--- api/metrics.py
+++ api/metrics.py
@@ -17,13 +17,13 @@ def process_gpu():
     all_process = list(map(Device.processes, all_devices))
     for device in all_process:
         for pid, gpu_process in device.items():
-            print(gpu_process.device.index)
-            print(pid)
-            print(gpu_process.status())
-            print(gpu_process.gpu_memory_human())
-            print(gpu_process.username())
-            print(gpu_process.command())
-            print(pprint_secs(gpu_process.host.create_time()))
+            # print(gpu_process.device.index)
+            # print(pid)
+            # print(gpu_process.status())
+            # print(gpu_process.gpu_memory_human())
+            # print(gpu_process.username())
+            # print(gpu_process.command())
+            # print(pprint_secs(gpu_process.host.create_time()))
             
             # tmp.append((pid, f'GPU:{gpu_process.device.index} PID:{pid} 状态:{gpu_process.status()} 显存占用:{gpu_process.gpu_memory_human()} 用户:{gpu_process.username()} 命令:{gpu_process.command()} 开始时间:{pprint_secs(gpu_process.host.create_time())} 运行时间:{gpu_process.host.running_time_human()}'))
             tmp.append((pid, gpu_process.device.index, gpu_process.status(), gpu_process.gpu_memory_human(), gpu_process.username(), gpu_process.command(), pprint_secs(gpu_process.host.create_time()), gpu_process.host.running_time_human()))
@@ -31,4 +31,5 @@ def process_gpu():
     return tmp
 
 if __name__ == "__main__":
-    process_gpu()
+    tmp = process_gpu()
+    print(tmp)
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from prometheus_client.core import CollectorRegistry
 from flask import Response, Flask
 from psutil import *
 from api.metrics import process_gpu
+import random
 app = Flask(__name__)
 
 REGISTRY = CollectorRegistry(auto_describe=False)
 info = Info(
     f"running_process",
     "Information about running processes.",
-    labelnames=['pid'],
+    labelnames=['pid_rand'],
     registry=REGISTRY)
 
 @app.route('/metrics')
@@ -19,8 +20,10 @@ def metrics():
     tmps = process_gpu()
     # 编写Prom语句并定义一个监控
     for pid, gpu_i, status, used, user, cmd, started, running_time in tmps:
+        rand_tmp = random.randint(0,1000)
         # 将参数传入监控项内
-        info.labels(f'{pid}').info({'GPU': str(gpu_i), 'status': str(status), 'used': str(used), 'user': str(user), 'cmd': str(cmd), 'started': str(started), 'running_time': str(running_time)})
+        info.labels(f'{pid}_{rand_tmp}').info({'pid': str(pid), 'GPU': str(gpu_i), 'status': str(status), 'used': str(used), 'user': str(user), 'cmd': str(cmd), 'started': str(started), 'running_time': str(running_time)})
+    print(info)
     return Response(prometheus_client.generate_latest(REGISTRY),
     mimetype="text/plain")
 
